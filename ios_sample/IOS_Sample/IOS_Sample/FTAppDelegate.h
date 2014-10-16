59dc23d93250f34d1813bbe75d1fad8a38388b9d
Ming Wang <wangmingjob@gmail.com>
2014-10-16 10:35:23 +0800

    Initial Commit

---
+++ ios_sample/IOS_Sample/IOS_Sample/FTAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  FTAppDelegate.h
+//  IOS_Sample
+//
+//  Created by Ming Wang on 10/16/14.
+//  Copyright (c) 2014 wangming. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FTAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ios_sample/IOS_Sample/IOS_Sample/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  IOS_Sample
+//
+//  Created by Ming Wang on 10/16/14.
+//  Copyright (c) 2014 wangming. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "FTAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([FTAppDelegate class]));
+    }
+}
