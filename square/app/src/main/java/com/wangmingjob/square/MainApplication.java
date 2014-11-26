c938277aaf73992923e6d6d1ededd8ef3629a38f
Ming Wang <wangmingjob@gmail.com>
2014-11-26 16:23:37 +0800

    add android kit

---
+++ square/app/src/main/java/com/wangmingjob/square/MainApplication.java
@@ -0,0 +1,14 @@
+package com.wangmingjob.square;
+
+import com.wangmingjob.androidkit.BaseApplication;
+
+/**
+ * Created by wangming on 11/25/14.
+ */
+public class MainApplication extends BaseApplication {
+
+	@Override
+	public void onCreate() {
+		super.onCreate();
+	}
+}
